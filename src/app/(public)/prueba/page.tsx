0e494a51725ea3eac3cc6f7a5610a2a1410fb616
alexander <alexander_pere@hotmail.com>
2025-05-14 11:41:52 -0500

    aasds

--- src/app/(public)/prueba/page.tsx
+++ src/app/(public)/prueba/page.tsx
@@ -1,4 +1,4 @@
-import { WorksViewModulo } from "@/modules/Works/WorksView";
+import { WorksViewModulo } from "../../../modules/Works/WorksView";
 
 export default function Prueba() {
   return <WorksViewModulo />;
