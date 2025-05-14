843f7e8842b6e9975316b60fdf1d4f603179fa3e
alexander <alexander_pere@hotmail.com>
2025-05-14 10:51:30 -0500

    ...

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,7 +12,11 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   ...compat.config({
-    extends: ['prettier'], // Solo prettier, next ya está incluido arriba
+    extends: ['prettier'],
+    rules: {
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
+    },
   }),
 ];
 
