dc1dd478973d393c61b2545aea552ffb3c50d2e0
alexander <alexander_pere@hotmail.com>
2025-05-24 23:28:29 -0500

    nav mejorado

--- src/layout/header/ActiveLink.tsx
+++ src/layout/header/ActiveLink.tsx
@@ -8,8 +8,13 @@ export const ActiveLink = ({ name, href, icon, active }: Nav & { active: boolean
     <Link
       href={href}
       className={cn(
-        'w-full text-xs font-medium text-white/70 transition-colors duration-300 ease-out hover:text-white',
+        'relative w-full text-xs font-medium text-white/70 uppercase transition-colors duration-300',
         active && 'text-white',
+
+        'after:absolute after:bottom-[-6px] after:h-[1px] after:bg-white/80 after:transition-all after:duration-500',
+        active
+          ? 'after:left-0 after:w-full'
+          : 'after:left-1/2 after:w-0 hover:after:left-0 hover:after:w-full',
       )}
     >
       {icon}
