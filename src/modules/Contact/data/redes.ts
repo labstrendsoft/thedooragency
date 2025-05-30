708d235bfccd876999bafb3f95575e33332b5793
alexander <alexander_pere@hotmail.com>
2025-05-30 17:17:23 -0500

    asds

--- src/modules/Contact/data/redes.ts
+++ src/modules/Contact/data/redes.ts
@@ -4,20 +4,20 @@ import igIcon from '@public/redes/ig.webp';
 import ytIcon from '@public/redes/yt.webp';
 
 export const socialLinks = [
-	{
-		name: 'Facebook',
-		url: 'https://www.facebook.com/agenciadepublicidadparabellezatrend',
-		icon: fbIcon,
-	},
-	{
-		name: 'Whatsapp',
-		url: 'https://instagram.com',
-		icon: wspIcon,
-	},
-	{
-		name: 'Instagram',
-		url: 'https://www.instagram.com/trend_belleza',
-		icon: igIcon,
-	},
-	{ name: 'Youtube', url: 'https://twitter.com', icon: ytIcon },
+  {
+    name: 'Facebook',
+    url: 'https://www.facebook.com/agenciadepublicidadparabellezatrend',
+    icon: fbIcon,
+  },
+  {
+    name: 'Whatsapp',
+    url: 'https://wa.me/519774353481?text=Hola%2C%20estoy%20interesado%20en%20conocer%20más%20sobre%20los%20servicios%20de%20su%20agencia.',
+    icon: wspIcon,
+  },
+  {
+    name: 'Instagram',
+    url: 'https://www.instagram.com/trend_belleza',
+    icon: igIcon,
+  },
+  { name: 'Youtube', url: 'https://twitter.com', icon: ytIcon },
 ];
